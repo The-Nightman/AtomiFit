47f5f50b757d9584b4dc44e1f3343b3735da9fd9
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-09-24 16:18:12 +0100

    add global exercise search and ExerciseListItem to app/exercisesSearch/categories.tsx

--- app/exercisesSearch/categories.tsx
+++ app/exercisesSearch/categories.tsx
@@ -7,6 +7,9 @@ import SearchBar from "@/components/SearchBar";
 import * as schema from "@/database/schema";
 import { hexcodeLuminosity } from "@/utils/hexcodeLuminosity";
 import { Category } from "@/types/categories";
+import { like } from "drizzle-orm";
+import { Exercise } from "@/types/exercise";
+import ExerciseListItem from "@/components/ExerciseListItem";
 
 /**
  * Categories component that displays a list of exercise categories.
@@ -20,41 +23,69 @@ import { Category } from "@/types/categories";
 const categories = (): JSX.Element => {
   const [categories, setCategories] = useState<Category[]>([]);
   const [search, setSearch] = useState<string>("");
+  const [searchResults, setSearchResults] = useState<Exercise[]>([]);
   const { db } = useContext(DrizzleContext);
 
+  // Fetch categories from the database on mount
   useEffect(() => {
     const data: Category[] = db.select().from(schema.categories).all();
     setCategories(data);
   }, []);
 
+  // Filter exercises based on the search
+  useEffect(() => {
+    // If search is empty, reset search results to show categories again and return early
+    if (!search) {
+      setSearchResults([]);
+      return;
+    }
+
+    // Query database for exercises LIKE search term, better UX than matching from start of name
+    const results: Exercise[] = db
+      .select()
+      .from(schema.exercises)
+      .where(like(schema.exercises.name, `%${search}%`))
+      .all();
+
+    setSearchResults(results);
+  }, [search]);
+
   return (
     <View>
       <SearchBar search={search} setSearch={setSearch} />
       <ScrollView>
-        {categories.map((category: Category) => (
-          <Pressable
-            key={category.id}
-            style={({ pressed }) => [
-              styles.categoryListItem,
-              pressed && { backgroundColor: "#595555" },
-            ]}
-          >
-            {/* indicator */}
-            <View
-              style={[
-                styles.categoryIndicator,
-                {
-                  backgroundColor: category.colour,
-                  borderColor: hexcodeLuminosity(category.colour, 40),
-                },
-              ]}
-            />
-            <Text style={styles.categoryText}>{category.name}</Text>
-            <Pressable>
-              <Entypo name="dots-three-vertical" size={28} color="#60DD49" />
-            </Pressable>
-          </Pressable>
-        ))}
+        {searchResults.length // If search results exist, display them, otherwise display categories
+          ? searchResults.map((exercise: Exercise) => (
+              <ExerciseListItem key={exercise.id} exercise={exercise} />
+            ))
+          : categories.map((category: Category) => (
+              <Pressable
+                key={category.id}
+                style={({ pressed }) => [
+                  styles.categoryListItem,
+                  pressed && { backgroundColor: "#595555" },
+                ]}
+              >
+                {/* indicator */}
+                <View
+                  style={[
+                    styles.categoryIndicator,
+                    {
+                      backgroundColor: category.colour,
+                      borderColor: hexcodeLuminosity(category.colour, 40),
+                    },
+                  ]}
+                />
+                <Text style={styles.categoryText}>{category.name}</Text>
+                <Pressable>
+                  <Entypo
+                    name="dots-three-vertical"
+                    size={28}
+                    color="#60DD49"
+                  />
+                </Pressable>
+              </Pressable>
+            ))}
       </ScrollView>
     </View>
   );
