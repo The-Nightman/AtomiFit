c2b0bbcd57a57881d684e11f7df38989d73c77f5
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-10-02 16:10:26 +0100

    modify Set interface with correct union types and remove accidental optionals

--- types/sets.ts
+++ types/sets.ts
@@ -2,9 +2,9 @@ export interface Set {
   id?: number;
   date: string;
   exercise_id: number;
-  weight?: number;
-  reps?: number;
-  distance?: number;
-  time?: number;
-  notes: string;
+  weight: number | null;
+  reps: number | null;
+  distance: number | null;
+  time: number | null;
+  notes: string | null;
 }
