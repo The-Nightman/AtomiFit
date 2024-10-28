394fb676888833a4f49863fa86a5faaffb45fd07
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-10-28 00:51:49 +0000

    refactor database query and processing logic for listView screen with a simpler query and only array.reduce for postprocessing for simplicity, readability and better maintainability

--- app/(calendar)/listView.tsx
+++ app/(calendar)/listView.tsx
@@ -2,19 +2,23 @@ import { DrizzleContext } from "@/contexts/drizzleContext";
 import { useContext, useEffect, useState } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import * as schema from "@/database/schema";
-import { desc, eq, sql } from "drizzle-orm";
-import { ListWorkout } from "@/types/listView";
+import { desc, eq } from "drizzle-orm";
+import { ListWorkout, ListWorkoutExercise } from "@/types/listView";
 import { FlatList } from "react-native-gesture-handler";
 import ListViewItem from "@/components/ListViewItem";
-import { Set } from "@/types/sets";
 import { getToday } from "@/utils/getToday";
 
 interface QueryResult {
+  id: number;
   date: string;
-  exercise_name: string;
-  category_name: string;
-  category_colour: string;
-  sets: string;
+  exercise_name: string | null;
+  category_name: string | null;
+  category_colour: string | null;
+  weight: number | null;
+  reps: number | null;
+  distance: number | null;
+  time: number | null;
+  notes: string | null;
 }
 
 /**
@@ -31,113 +35,132 @@ const ListView = (): JSX.Element => {
   const today = getToday(); // Get the current date
 
   useEffect(() => {
-    // Query the database for the data
-    const data: QueryResult[] = db
-      .select({
-        date: schema.setsData.date,
-        exercise_name: schema.exercises.name,
-        category_name: schema.categories.name,
-        category_colour: schema.categories.colour,
-        sets: sql`'[' || GROUP_CONCAT(
-          JSON_OBJECT(
-          'id', sets_data.id,
-          'weight', sets_data.weight,
-          'reps', sets_data.reps,
-          'distance', sets_data.distance,
-          'time', sets_data.time,
-          'notes', sets_data.notes
-          )
-        ) || ']' AS sets`,
-      })
-      .from(schema.setsData)
-      .leftJoin(
-        schema.exercises,
-        eq(schema.setsData.exercise_id, schema.exercises.id)
-      )
-      .leftJoin(
-        schema.categories,
-        eq(schema.exercises.category_id, schema.categories.id)
-      )
-      .groupBy(
-        schema.setsData.date,
-        schema.exercises.name,
-        schema.categories.name,
-        schema.categories.colour
-      )
-      .orderBy(desc(schema.setsData.date))
-      .all() as unknown as QueryResult[];
+    /**
+     * Fetches workout data from the database, processes it into a structured format,
+     * and updates the state with the processed data. The data is grouped by date and
+     * then sub-grouped by exercise. If there is no data for today, a placeholder element
+     * is added.
+     *
+     * @async
+     * @returns {Promise<void>} A promise that resolves when the data has been fetched and processed.
+     */
+    const fetchData = async (): Promise<void> => {
+      const data: QueryResult[] = await db
+        .select({
+          id: schema.setsData.id,
+          date: schema.setsData.date,
+          exercise_name: schema.exercises.name,
+          category_name: schema.categories.name,
+          category_colour: schema.categories.colour,
+          weight: schema.setsData.weight,
+          reps: schema.setsData.reps,
+          distance: schema.setsData.distance,
+          time: schema.setsData.time,
+          notes: schema.setsData.notes,
+        })
+        .from(schema.setsData)
+        .leftJoin(
+          schema.exercises,
+          eq(schema.setsData.exercise_id, schema.exercises.id)
+        )
+        .leftJoin(
+          schema.categories,
+          eq(schema.exercises.category_id, schema.categories.id)
+        )
+        .orderBy(desc(schema.setsData.date));
 
-    // Group the data by date
-    const groupedData = data.reduce(
-      (
-        acc: {
-          [key: string]: {
-            exercise_name: string;
-            category_name: string;
-            category_colour: string;
-            sets: Set[];
-          }[];
-        },
-        item
-      ) => {
-        const date: string = item.date;
-        const sets: Set[] = JSON.parse(item.sets);
-
-        // If the date object doesn't exist in the acc initialize it
-        if (!acc[date]) {
-          acc[date] = [];
+      // Process the data into the ListWorkout type grouping by date and then sub-group by exercise
+      const processedData = data.reduce<ListWorkout[]>((acc, item) => {
+        const dateGroup: ListWorkout | undefined = acc.find(
+          (accItem) => accItem.date === item.date
+        );
+        // Check if the date group exists in the accumulator
+        if (dateGroup) {
+          const exerciseGroup: ListWorkoutExercise | undefined =
+            dateGroup.data.find(
+              (dateItem) => dateItem.exercise_name === item.exercise_name
+            );
+          // If date group exists, check if the exercise group exists in the date group
+          if (exerciseGroup) {
+            exerciseGroup.sets.push({
+              id: item.id,
+              weight: item.weight,
+              reps: item.reps,
+              distance: item.distance,
+              time: item.time,
+              notes: item.notes,
+            });
+            // If exercise group does not exist, create a new exercise group and add the set
+          } else {
+            dateGroup.data.push({
+              exercise_name: item.exercise_name!,
+              category_name: item.category_name!,
+              category_colour: item.category_colour!,
+              sets: [
+                {
+                  id: item.id,
+                  weight: item.weight,
+                  reps: item.reps,
+                  distance: item.distance,
+                  time: item.time,
+                  notes: item.notes,
+                },
+              ],
+            });
+          }
+          // If date group does not exist, create a new date group and exercise group and add the set
+        } else {
+          acc.push({
+            date: item.date,
+            data: [
+              // Exercise group object
+              {
+                exercise_name: item.exercise_name!,
+                category_name: item.category_name!,
+                category_colour: item.category_colour!,
+                sets: [
+                  // Set object
+                  {
+                    id: item.id,
+                    weight: item.weight,
+                    reps: item.reps,
+                    distance: item.distance,
+                    time: item.time,
+                    notes: item.notes,
+                  },
+                ],
+              },
+            ],
+          });
         }
 
-        // Push the data into the object
-        acc[date].push({
-          exercise_name: item.exercise_name,
-          category_name: item.category_name,
-          category_colour: item.category_colour,
-          sets: sets,
-        });
-
-        // Sort the data by the first set id
-        // This guarantees that the data is sorted by the order it was logged by user
-        acc[date].sort((a, b) => {
-          const firstSetA: number = a.sets[0].id!;
-          const firstSetB: number = b.sets[0].id!;
-          return firstSetA - firstSetB;
-        });
-
         return acc;
-      },
-      {}
-    );
+      }, []);
 
-    // Format the data into the an array of the ListWorkout type
-    const formattedData: ListWorkout[] = Object.keys(groupedData).map(
-      (date: string) => ({
-        date: date,
-        data: groupedData[date],
-      })
-    );
+      // Check if there is data for today, if not add a placeholder element
+      if (!processedData.some((item) => item.date === today)) {
+        setData(() => {
+          // If there is no data for today, add a placeholder element and sort
+          const data = [...processedData, { date: today, data: [] }].sort(
+            (a, b) => {
+              // Sort the data by date descending, we use getTime() to compare the dates as integers
+              return new Date(b.date).getTime() - new Date(a.date).getTime();
+            }
+          );
+          // Return the data to state
+          return data;
+        });
+      } else {
+        setData(processedData);
+      }
+    };
 
-    // Check if there is data for today
-    if (!formattedData.some((item) => item.date === today)) {
-      setData(() => {
-        // If there is no data for today, add a placeholder element and sort
-        const data = [...formattedData, { date: today, data: [] }].sort(
-          (a, b) => {
-            // Sort the data by date descending, we use getTime() to compare the dates as integers
-            return new Date(b.date).getTime() - new Date(a.date).getTime();
-          }
-        );
-        // Return the data to state
-        return data;
-      });
-    } else {
-      setData(formattedData);
-    }
+    fetchData();
   }, []);
 
   /**
    * Render the list item elements for the listview.
-   * 
+   *
    * Renders a placeholder element if the item date property is the current date
    * and data array is empty. Renders the ListViewItem component otherwise.
    *
