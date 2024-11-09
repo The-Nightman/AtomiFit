afd5b9a586b975b6d0e04a432079b4d7db92ea45
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-11-09 02:22:55 +0000

    add types for various exercise linegraph options

--- types/graphs.ts
+++ types/graphs.ts
@@ -1,16 +1,56 @@
 export interface LineGraphOptions {
   selectedGraph:
-    | "oneRepMax"
-    | "maxWeight"
-    | "maxReps"
-    | "maxVolume"
-    | "maxWeightReps"
-    | "workoutVolume"
-    | "workoutReps"
-    | "personalRecords";
+    | WeightRepsOptions
+    | DistanceTimeOptions
+    | WeightTimeOptions
+    | WeightDistanceOptions
+    | WeightOptions
+    | TimeOptions
+    | RepsTimeOptions
+    | RepsDistanceOptions
+    | RepsOptions
+    | DistanceOptions;
   startDate: "1M" | "3M" | "6M" | "1Y" | "ALL" | "string";
   endDate: string;
 }
+
+type WeightRepsOptions =
+  | "oneRepMax"
+  | "maxWeight"
+  | "maxReps"
+  | "maxVolume"
+  | "maxWeightReps"
+  | "workoutVolume"
+  | "workoutReps"
+  | "personalRecords";
+
+type DistanceTimeOptions =
+  | "maxDistance"
+  | "maxTime"
+  | "maxSpeed"
+  | "maxPace"
+  | "workoutDistance"
+  | "workoutTime";
+
+type WeightTimeOptions = "maxWeight" | "maxTime" | "workoutTime";
+
+type WeightDistanceOptions = "maxWeight" | "maxDistance" | "workoutDistance";
+
+type WeightOptions = "maxWeight";
+
+type TimeOptions = "maxTime" | "workoutTime";
+
+type RepsTimeOptions = "maxReps" | "workoutReps" | "maxTime" | "workoutTime";
+
+type RepsDistanceOptions =
+  | "maxReps"
+  | "workoutReps"
+  | "maxDistance"
+  | "workoutDistance";
+
+type RepsOptions = "maxReps" | "workoutReps";
+
+type DistanceOptions = "maxDistance" | "workoutDistance";
 
 export interface ExerciseGraphData {
   id: number;
