62ea1fdb504697719ddabd25d692c1918c6dfb81
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-12-15 18:09:38 +0000

    add support for units to app/exercise/[exerciseId]/track screen

--- app/exercise/[exerciseId]/track.tsx
+++ app/exercise/[exerciseId]/track.tsx
@@ -16,6 +16,7 @@ import Animated, {
   withTiming,
 } from "react-native-reanimated";
 import { useLiveQuery } from "drizzle-orm/expo-sqlite";
+import { Unit } from "@/types/units";
 
 /**
  * Track component.
@@ -36,9 +37,10 @@ const Track = (): JSX.Element => {
     pos: { x: 0, y: 0 },
     currentSelectedId: null,
   });
-  const { exerciseId, exerciseType, date } = useLocalSearchParams<{
+  const { exerciseId, exerciseType, unit, date } = useLocalSearchParams<{
     exerciseId: string;
     exerciseType: string;
+    unit: string; // see @/types/units, we must cast as string here due to being url params so we will cast as Unit later
     date: string;
   }>();
   const { db } = useContext(DrizzleContext);
@@ -130,6 +132,7 @@ const Track = (): JSX.Element => {
         date: date,
         ...setTemplates[exerciseType],
         notes: "",
+        unit: unit === "null" ? null : unit as Unit,
       };
 
       // Insert the new set
