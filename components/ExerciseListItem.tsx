c4b66f382f0f736667256aee6163418204c26fef
Keenan Brant <keenan.brant@yahoo.co.uk>
2024-12-25 15:54:56 +0000

    switch the router navigation method in ExerciseListItem to router.replace for the desired navigation behaviour

--- components/ExerciseListItem.tsx
+++ components/ExerciseListItem.tsx
@@ -13,6 +13,8 @@ interface ExerciseListItemProps {
 
 /**
  * Component representing a single item in an exercise list on the browse screens.
+ * 
+ * The component navigates to the exercise screen via the replace method on press.
  *
  * @component
  * @param {ExerciseListItemProps} props - The properties for the component.
@@ -89,7 +91,8 @@ const ExerciseListItem = ({
   return (
     <Pressable
       onPress={() =>
-        router.push({
+        // We need to use replace for the navigation behavior we want, for some reason certain router methods are not available?
+        router.replace({
           pathname: "/exercise/[exerciseId]/track",
           params: { exerciseId: exercise.id, date: date },
         })
@@ -108,11 +111,7 @@ const ExerciseListItem = ({
           eventEmitter.emit("exerciseMenu", exercise.id, position);
         }}
       >
-        <Entypo
-          name="dots-three-vertical"
-          size={28}
-          color="#60DD49"
-        />
+        <Entypo name="dots-three-vertical" size={28} color="#60DD49" />
       </Pressable>
     </Pressable>
   );
