a87d030d9be13aca231bdcfcd7298af8c3a28796
Keenan Brant <keenan.brant@yahoo.co.uk>
2025-01-04 01:05:45 +0000

    move header elements into stack header prop and add newCategory screen to stack in app/exercises/create

--- app/exercises/create/_layout.tsx
+++ app/exercises/create/_layout.tsx
@@ -1,13 +1,13 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { router, Stack } from "expo-router";
 import { MaterialIcons } from "@expo/vector-icons";
-import UtilityStyles from "@/constants/UtilityStyles";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { eventEmitter } from "@/utils/eventEmitter";
 import { hexcodeLuminosity } from "@/utils/hexcodeLuminosity";
+import { usePathname } from "expo-router/build/hooks";
 
 /**
- * ExerciseCreateLayout component renders the layout for creating a new exercise.
+ * ExerciseCreateLayout component renders the layout for creating a new exercise or category.
  * It includes a header with a close button, a title, and a check/confirm button.
  * The main content is displayed within a Stack navigator.
  *
@@ -15,10 +15,26 @@ import { hexcodeLuminosity } from "@/utils/hexcodeLuminosity";
  */
 const ExerciseCreateLayout = (): JSX.Element => {
   const insets = useSafeAreaInsets();
+  const screen = usePathname().match(/^\/exercises\/create\/(.*)$/)![1]; // this comes from the router so it will always be present
 
-  return (
-    <View style={UtilityStyles.flex1}>
-      <View style={[styles.headerContainer, { paddingTop: insets.top }]}>
+  /**
+   * Renders the header component for the create exercise/category screen.
+   *
+   * The header includes:
+   * - A close button that navigates back to the previous screen.
+   * - A title that dynamically displays "Add New Exercise" or "Add New Category" based on the `screen` prop.
+   * - A check button that triggers the creation of a new exercise or category based on the `screen` prop.
+   *
+   * @returns {JSX.Element} The header component.
+   */
+  const header = (): JSX.Element => {
+    return (
+      <View
+        style={[
+          styles.headerContainer,
+          { paddingTop: insets.top, zIndex: 1000 },
+        ]}
+      >
         <Pressable
           style={({ pressed }) => [
             { borderRadius: 22 },
@@ -39,7 +55,7 @@ const ExerciseCreateLayout = (): JSX.Element => {
           )}
         </Pressable>
         <Text style={{ fontSize: 28, fontWeight: "500" }}>
-          Add New Exercise
+          Add New {screen === "newExercise" ? "Exercise" : "Category"}
         </Text>
         <Pressable
           style={({ pressed }) => [
@@ -48,7 +64,15 @@ const ExerciseCreateLayout = (): JSX.Element => {
               backgroundColor: `${hexcodeLuminosity("#3F3C3C", 30)}66`,
             },
           ]}
-          onPress={() => eventEmitter.emit("createExercise")}
+          onPress={() => {
+            if (screen) {
+              if (screen === "newCategory") {
+                eventEmitter.emit("createCategory");
+              } else {
+                eventEmitter.emit("createExercise");
+              }
+            }
+          }}
         >
           {({ pressed }) => (
             <MaterialIcons
@@ -59,15 +83,19 @@ const ExerciseCreateLayout = (): JSX.Element => {
           )}
         </Pressable>
       </View>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-          contentStyle: { backgroundColor: "#0F0F0F" },
-        }}
-      >
-        <Stack.Screen name="newExercise" options={{ gestureEnabled: true }} />
-      </Stack>
-    </View>
+    );
+  };
+
+  return (
+    <Stack
+      screenOptions={{
+        header: () => header(),
+        contentStyle: { backgroundColor: "#0F0F0F" },
+      }}
+    >
+      <Stack.Screen name="newExercise" options={{ gestureEnabled: true }} />
+      <Stack.Screen name="newCategory" options={{ gestureEnabled: true }} />
+    </Stack>
   );
 };
 
