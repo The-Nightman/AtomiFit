74402b553badadd0da701f57d69ba979233a3f29
Keenan Brant <keenan.brant@yahoo.co.uk>
2025-01-08 16:51:45 +0000

    refactor updateSetting function in settingsContext so that state will be updated appropriately if called sequentialy or in a loop

--- contexts/settingsContext.tsx
+++ contexts/settingsContext.tsx
@@ -64,7 +64,7 @@ export const SettingsProvider = ({ children }: SettingsProviderProps) => {
       for (const [key, value] of Object.entries(defaultSettings)) {
         const existingKey = await Storage.getItem(key);
         if (!existingKey) {
-          await Storage.setItem(key, value); 
+          await Storage.setItem(key, value);
         }
       }
 
@@ -100,7 +100,12 @@ export const SettingsProvider = ({ children }: SettingsProviderProps) => {
   ): Promise<void> => {
     await Storage.setItem(key, value.toString()); // We need to convert the value to a string as the storage only accepts strings
     if (appSettings) {
-      setAppSettings({ ...appSettings, [key]: value });
+      // We need to use the previous state for our updater function or else our settings
+      // updates will be batched together if we call this func in a for of/for await loop
+      setAppSettings((prevState) => ({
+        ...(prevState as AppSettings),
+        [key]: value,
+      }));
     }
   };
 
