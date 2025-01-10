c4743a99c0adfe1c0a40ac13f4b94c06d8cc1dbc
Keenan Brant <keenan.brant@yahoo.co.uk>
2025-01-10 00:32:32 +0000

    refactor getToday and displayDate util functions to use ISO 8601 date time strings, this actually already accounts for daylight savings

--- utils/getToday.ts
+++ utils/getToday.ts
@@ -1,13 +1,10 @@
 /**
- * Returns the current date in the format "YYYY-MM-DD".
+ * Returns the current date in ISO 8601 format.
  *
  * @returns {string} The current date.
  */
 export const getToday = (): string => {
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-
-  return `${year}-${month}-${day}`;
+  const dateObj = new Date();
+  dateObj.setHours(0, 0, 0, 0);
+  return dateObj.toISOString();
 };
