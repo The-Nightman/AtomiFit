8dd5ce08a3569d213150612628dca701a8966cdb
Keenan Brant <keenan.brant@yahoo.co.uk>
2025-03-01 21:51:35 +0000

    update Exercise interface with new explicit typed ExerciseTypes type

--- types/exercise.ts
+++ types/exercise.ts
@@ -1,10 +1,23 @@
 import { WeightUnit } from "./units";
 
+export type ExerciseTypes =
+  | "Weight And Reps"
+  | "Distance And Time"
+  | "Weight And Distance"
+  | "Weight And Time"
+  | "Reps And Distance"
+  | "Reps And Time"
+  | "Weight"
+  | "Reps"
+  | "Distance"
+  | "Time";
+
 export interface Exercise {
   category_id: number;
   id: number;
   name: string;
   notes: string;
-  type: string;
+  type: ExerciseTypes;
+  mp4Url: string;
   weight_unit: WeightUnit;
 }
