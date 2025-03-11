bb866747300aad0c601d9515b84b52112c3156b0
Keenan Brant <keenan.brant@yahoo.co.uk>
2025-03-11 18:42:25 +0000

    refactor insertNextBestSet util function to prevent unique errors

--- utils/db/insertNextBestSet.ts
+++ utils/db/insertNextBestSet.ts
@@ -1,25 +1,47 @@
 import { and, asc, desc, eq, ne } from "drizzle-orm";
 import { ExpoSQLiteDatabase } from "drizzle-orm/expo-sqlite";
 import * as schema from "@/database/schema";
+import { SetPersonalRecord } from "@/types/sets";
 
+interface PrevPrSet {
+  set_id: number;
+  exercise_id: number;
+  set_reps: number;
+}
+
+/**
+ * Inserts the next best set into the personal records if it meets the criteria.
+ *
+ * @param {ExpoSQLiteDatabase<Record<string, never>>} db - The database instance to perform the operations on.
+ * @param {PrevPrSet} prevPrSet - The previous personal record set containing set_id, exercise_id, and set_reps.
+ * @param {number} id - The ID of the current set to exclude from the search.
+ *
+ * @returns A promise that resolves when the operation is complete.
+ *
+ * This function searches for the next best set with the same exercise_id and set_reps as the previous personal record set,
+ * but with a different set_id. It orders the results by weight in descending order, and by date and id in ascending order,
+ * to find the most recent but earliest set that matches the criteria. If a matching set is found and it is not already in the personal records,
+ * it inserts the set into the personal records.
+ */
 export const insertNextBestSet = async (
   db: ExpoSQLiteDatabase<Record<string, never>>,
-  prevPrSet: {
-    set_id: number;
-    exercise_id: number;
-    set_reps: number | null;
-  },
+  prevPrSet: PrevPrSet,
   id: number
 ) => {
   // Search for the next best set with the same reps criteria
-  const nextBestSet: ({ id: number; exercise_id: number } | null)[] = await db
+  const [nextBestSet]: ({ id: number; exercise_id: number; weight: number | null; date: string; personal_record: SetPersonalRecord['personal_record'] } | null)[] = await db
     .select({
       id: schema.setsData.id,
       exercise_id: schema.setsData.exercise_id,
       weight: schema.setsData.weight,
       date: schema.setsData.date,
+      personal_record: schema.personalRecords,
     })
     .from(schema.setsData)
+    .leftJoin(
+      schema.personalRecords,
+      eq(schema.setsData.id, schema.personalRecords.set_id)
+    )
     .where(
       and(
         eq(schema.setsData.exercise_id, prevPrSet.exercise_id),
@@ -65,10 +87,10 @@ export const insertNextBestSet = async (
     .limit(1);
 
   // If there is a next best set, insert it into the personal records
-  if (nextBestSet[0]) {
+  if (nextBestSet && !nextBestSet.personal_record?.id) {
     await db.insert(schema.personalRecords).values({
-      set_id: nextBestSet[0].id,
-      exercise_id: nextBestSet[0].exercise_id,
+      set_id: nextBestSet.id,
+      exercise_id: nextBestSet.exercise_id,
     });
   }
 };
