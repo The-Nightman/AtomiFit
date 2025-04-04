eb69131fa4439bb3e5c017c2b78ce8a282f64187
Keenan Brant <keenan.brant@yahoo.co.uk>
2025-04-04 01:26:22 +0100

    add notification handler and notifications channels and categories setup and add TimerProvider context provider in app/_layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -16,9 +16,74 @@ import { version } from "../package.json";
 import { Storage } from "expo-sqlite/kv-store";
 import { StatusBar } from "expo-status-bar";
 import NotesModal from "@/components/modals/NotesModal";
-import { View } from "react-native";
+import { Platform, View } from "react-native";
+import * as Notifications from "expo-notifications";
+import { TimerProvider } from "@/contexts/timerContext";
+import {
+  createNotificationChannel,
+  createNotificationCategories,
+} from "@/utils/notifications/setupNotifications";
 
 const APP_VERSION = version;
+
+type NotificationDataFix = Notifications.NotificationContent &
+  (
+    | Notifications.NotificationContentIos
+    | Notifications.NotificationContentAndroid
+  ) & {
+    dataString: DataString;
+  };
+
+interface DataString {
+  shouldPlaySound: boolean;
+}
+
+Notifications.setNotificationHandler({
+  handleNotification: async (notification) => {
+    const notifContent = notification.request.content as NotificationDataFix;
+
+    // The data object will always be present if passed on iOS
+    if (Platform.OS === "ios") {
+      return {
+        shouldShowAlert: true,
+        shouldPlaySound: notifContent.data?.shouldPlaySound ?? true,
+        shouldSetBadge: true,
+      };
+    }
+
+    // For some reason the data object in NotificationContent on android risks being swapped with a stringified
+    // JSON object named dataString and this is neither included in the interface or the documentation.
+    // Because of this we need to check for both and if neither are present we should set default behavior
+    if (notifContent.data) {
+      return {
+        shouldShowAlert: true,
+        shouldPlaySound: notifContent.data.shouldPlaySound ?? true,
+        shouldSetBadge: true,
+      };
+    }
+    if (notifContent.dataString) {
+      return {
+        shouldShowAlert: true,
+        shouldPlaySound:
+          JSON.parse(notifContent.dataString as unknown as string)
+            .shouldPlaySound ?? true,
+        shouldSetBadge: true,
+      };
+    }
+
+    // Edge case for when no data object is not passed in the request, we should mimic default behavior
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+    };
+  },
+});
+
+// Notifications channels and categories should be set up outside of the
+// component lifecycle to make sure we dont end up with duplicates
+createNotificationChannel();
+createNotificationCategories();
 
 const RootLayoutNav = () => {
   const { db } = useContext(DrizzleContext);
@@ -67,42 +132,47 @@ const RootLayoutNav = () => {
   return (
     <DrizzleProvider>
       <SettingsProvider>
-        <GestureHandlerRootView>
-          <StatusBar
-            style="light" //We may want to change this to dark depending on the theme later
-            translucent
-          />
-          <Stack
-            screenOptions={{
-              headerShown: false,
-              contentStyle: { backgroundColor: "#0F0F0F" },
-            }}
-          >
-            <Stack.Screen name="index" options={{ gestureEnabled: true }} />
-            <Stack.Screen name="welcome" options={{ gestureEnabled: true }} />
-            <Stack.Screen name="timeline" options={{ gestureEnabled: true }} />
-            <Stack.Screen
-              name="exercises"
-              options={{
-                gestureEnabled: true,
-                gestureDirection: "vertical",
-                animation: "slide_from_bottom",
+        <TimerProvider>
+          <GestureHandlerRootView>
+            <StatusBar
+              style="light" //We may want to change this to dark depending on the theme later
+              translucent
+            />
+            <Stack
+              screenOptions={{
+                headerShown: false,
+                contentStyle: { backgroundColor: "#0F0F0F" },
               }}
-            />
-            <Stack.Screen
-              name="exercise/[exerciseId]"
-              options={{ gestureEnabled: true }}
-            />
-          </Stack>
-          {/* 
+            >
+              <Stack.Screen name="index" options={{ gestureEnabled: true }} />
+              <Stack.Screen name="welcome" options={{ gestureEnabled: true }} />
+              <Stack.Screen
+                name="timeline"
+                options={{ gestureEnabled: true }}
+              />
+              <Stack.Screen
+                name="exercises"
+                options={{
+                  gestureEnabled: true,
+                  gestureDirection: "vertical",
+                  animation: "slide_from_bottom",
+                }}
+              />
+              <Stack.Screen
+                name="exercise/[exerciseId]"
+                options={{ gestureEnabled: true }}
+              />
+            </Stack>
+            {/* 
             This does look misplaced and kind of hacky but the notes modal is used across the app,
             also it needs to be wrapped by a view or else it breaks the styling and is unuseable.
             regardless as long as the events are emitted correctly there wont be a problem
           */}
-          <View>
-            <NotesModal />
-          </View>
-        </GestureHandlerRootView>
+            <View>
+              <NotesModal />
+            </View>
+          </GestureHandlerRootView>
+        </TimerProvider>
       </SettingsProvider>
     </DrizzleProvider>
   );
